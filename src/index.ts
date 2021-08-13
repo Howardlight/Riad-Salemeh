b5f9b1c273defb65925ae178bf344585d5063164
Ciel/howard <67962926+Howardlight@users.noreply.github.com>
2021-08-13 23:21:53 +0300

    Added onMessage handler

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Client, Intents } from "discord.js";
 import { validateEnv } from "./utils/validateEnv";
-
+import { onMessage } from "./events/onMessage";
 
 (async () => {
 
@@ -8,12 +8,16 @@ import { validateEnv } from "./utils/validateEnv";
     // if assertion fails, stops the bot
     if(!validateEnv) return;
 
-    const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES]})
+    const client = new Client(
+        { intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES]}
+        );
 
     client.once("ready", ()=> {
         client.user?.setActivity("with your Dollars");
         console.log(`Logged in as ${client.user?.username}! | ${client.user?.id}`);
     });
+
+    client.on("messageCreate", async (message) => await onMessage(message));
 
 
     client.login(process.env.TOKEN);
