867ad00ed2faba358fb53714edbff7a6987d811b
Ciel/howard <67962926+Howardlight@users.noreply.github.com>
2021-08-13 23:32:14 +0300

    Added github Command to CommandList

--- src/commands/_CommandList.ts
+++ src/commands/_CommandList.ts
@@ -1,4 +1,4 @@
 import { CommandInt } from "../interfaces/CommandInt";
+import { github } from "./github";
 
-
-export const CommandList: CommandInt[] = [];
+export const CommandList: CommandInt[] = [github];
