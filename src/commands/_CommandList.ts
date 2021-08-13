aa421059cc26782013bca44d7ad157d28becda5d
Ciel/howard <67962926+Howardlight@users.noreply.github.com>
2021-08-13 23:36:12 +0300

    Added Link Command to CommandList

--- src/commands/_CommandList.ts
+++ src/commands/_CommandList.ts
@@ -1,4 +1,7 @@
 import { CommandInt } from "../interfaces/CommandInt";
 import { github } from "./github";
+import { link } from "./link";
 
-export const CommandList: CommandInt[] = [github];
+
+
+export const CommandList: CommandInt[] = [github, link];
