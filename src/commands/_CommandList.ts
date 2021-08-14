d75ff8f6b450b43400ac7fa800a8d66b2fd3e84a
Ciel/howard <67962926+Howardlight@users.noreply.github.com>
2021-08-14 21:39:02 +0300

    Added Lbprate Command

--- src/commands/_CommandList.ts
+++ src/commands/_CommandList.ts
@@ -2,6 +2,7 @@ import { CommandInt } from "../interfaces/CommandInt";
 import { github } from "./github";
 import { link } from "./link";
 import { list } from "./list";
+import { lbprate } from "./lbprate";
 
 
-export const CommandList: CommandInt[] = [github, link, list];
+export const CommandList: CommandInt[] = [github, link, list, lbprate];
