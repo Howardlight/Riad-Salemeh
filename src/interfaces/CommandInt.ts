154fb1b4089017f8f9dab8aa5324160d0c1d5334
Ciel/howard <67962926+Howardlight@users.noreply.github.com>
2021-08-14 23:18:24 +0300

    Expanded CommandInterface to handle optional args

--- src/interfaces/CommandInt.ts
+++ src/interfaces/CommandInt.ts
@@ -1,10 +1,35 @@
 import { Message } from "discord.js";
 
-export interface CommandInt {
+export interface BaseCommandInt {
     name: string;
     description: string;
     cooldown?: number;
     usage?: string;
 
+    // run: ((message: Message) => Promise<void>) 
+    // | ((message: Message, args: string[]) => Promise<void>)
+    // | ((message: Message, args?: string[]) => Promise<void>) ;
+
+}
+
+//  NOTE // 
+// Since Interfaces cannot harbor Logic,
+// one way to endure logic is to split and create
+// interfaces based on the logic
+
+// CommandInt is for non Arg Commands
+export interface CommandIntNoArgs extends BaseCommandInt {
     run: (message: Message) => Promise<void>;
 }
+
+// For Command that REQUIRE ARGS
+export interface CommandIntArgs extends BaseCommandInt {
+    run: (message: Message, args: string[]) => Promise<void>;
+}
+
+// For Commands where Args are OPTIONAL
+export interface CommandIntOptionalArgs extends BaseCommandInt {
+    run: (message: Message, args?: string[]) => Promise<void>;
+}
+
+export type CommandInt = CommandIntNoArgs | CommandIntArgs | CommandIntOptionalArgs;
