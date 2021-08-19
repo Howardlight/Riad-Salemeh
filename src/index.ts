93805dbf05c51193d17c3cfb55657b407adad4e6
Ciel/howard <67962926+Howardlight@users.noreply.github.com>
2021-08-19 23:52:05 +0300

    Added Cooldowns Collection

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
-import { Client, Intents } from "discord.js";
+import { Client, Intents, Collection } from "discord.js";
 import { validateEnv } from "./utils/validateEnv";
 import { onMessage } from "./events/onMessage";
 
+
+export const cooldowns = new Collection();
 (async () => {
+
+
+    //TODO: Add mv -i -v public dist/ && rm -v -rf src to postInstall
+    // CHECK : https://medium.com/developer-rants/deploying-typescript-node-js-applications-to-heroku-81dd75424ce0
+    //TODO: Remove Build or postinstall, since heroku ends up building the bot TWICE
 
     // Validates Env variables
     // if assertion fails, stops the bot
@@ -12,7 +19,7 @@ import { onMessage } from "./events/onMessage";
 
     const client = new Client(
         { intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES]}
-        );
+    );
 
     client.once("ready", ()=> {
         client.user?.setActivity("with your Dollars");
