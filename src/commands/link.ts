cb2a177844d5adf3b7abd79687af774642c67615
Ciel/howard <67962926+Howardlight@users.noreply.github.com>
2021-08-24 20:58:25 +0300

    Added outdated marker

--- src/commands/link.ts
+++ src/commands/link.ts
@@ -6,6 +6,8 @@ export const link: CommandInt = {
     description: "Returns an invite link to the bot, which can be used to invite the bot to servers",
     run: async (message: Message) => {
         message.channel.send(
+            //OUTDATED
+            //TODO: update this
             "https://discord.com/oauth2/authorize?client_id=826815896718540850&scope=bot&permissions=36826705"
         );
     }
