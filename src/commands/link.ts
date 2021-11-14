0beca679647ea008965f6b8e270d4045868d4666
Ciel/howard <howardlight+github@protonmail.com>
2021-11-14 20:11:44 +0200

    Revamped Link command with an Embed

--- src/commands/link.ts
+++ src/commands/link.ts
@@ -1,12 +1,18 @@
-import { Message } from "discord.js";
+import { Message, MessageEmbed } from "discord.js";
 import { CommandInt } from "../interfaces/CommandInt";
 
 export const link: CommandInt = {
     name: "link",
     description: "Returns an invite link to the bot, which can be used to invite the bot to servers",
     run: async (message: Message) => {
-        message.channel.send(
-            "https://discord.com/api/oauth2/authorize?client_id=826815896718540850&permissions=2147535872&scope=bot%20applications.commands"
-        );
+
+        const Embed = new MessageEmbed()
+        .setDescription("[Click Me](https://discord.com/api/oauth2/authorize?client_id=826815896718540850&permissions=2147535872&scope=bot%20applications.commands) to invite the bot!")
+        .addFields(
+            // { name: '\u200B', value: '\u200B' },
+            { name: "Or copy and paste the following link to your browser:", value: '`https://discord.com/api/oauth2/authorize?client_id=826815896718540850&permissions=2147535872&scope=bot%20applications.commands`' }
+        )
+        
+        message.channel.send({ embeds: [Embed ]});
     }
 }
--- src/interactions/link.ts
+++ src/interactions/link.ts
@@ -1,12 +1,20 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
-import { CommandInteraction } from "discord.js";
+import { CommandInteraction, MessageEmbed } from "discord.js";
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('link')
 		.setDescription('Returns link to invite the bot'),
 	async execute(interaction: CommandInteraction) {
+
+		const Embed = new MessageEmbed()
+        .setDescription("[Click Me](https://discord.com/api/oauth2/authorize?client_id=826815896718540850&permissions=2147535872&scope=bot%20applications.commands) to invite the bot!")
+        .addFields(
+            // { name: '\u200B', value: '\u200B' },
+            { name: "Or copy and paste the following link to your browser:", value: '`https://discord.com/api/oauth2/authorize?client_id=826815896718540850&permissions=2147535872&scope=bot%20applications.commands`' }
+        )
+
 		// https://discord.com/oauth2/authorize?client_id=826815896718540850&scope=bot&permissions=36826705
-        await interaction.reply(`https://discord.com/api/oauth2/authorize?client_id=826815896718540850&permissions=2147535872&scope=bot%20applications.commands`);
+        await interaction.reply({ embeds: [Embed]});
 	},
 };
