e98439ed2604870dd592fbb13c991b7227114202
Ciel/howard <howardlight+github@protonmail.com>
2021-11-14 19:05:54 +0200

    Removed Seconds and millisecs from Uptime

--- src/interactions/getUptime.ts
+++ src/interactions/getUptime.ts
@@ -13,20 +13,17 @@ module.exports = {
         const date = new Date(uptime*1000);
         const days = date.getUTCDate() - 1,
               hours = date.getUTCHours(),
-              minutes = date.getUTCMinutes(),
-              seconds = date.getUTCSeconds(),
-              milliseconds = date.getUTCMilliseconds();
+              minutes = date.getUTCMinutes();
 
         let segments = [];
 
         if (days > 0) segments.push(days + ' day' + ((days == 1) ? '' : 's'));
         if (hours > 0) segments.push(hours + ' hour' + ((hours == 1) ? '' : 's'));
-        if (minutes > 0) segments.push(minutes + ' minute' + ((minutes == 1) ? '' : 's'));
-        if (seconds > 0) segments.push(seconds + ' second' + ((seconds == 1) ? '' : 's'));
-        if (milliseconds > 0) segments.push(milliseconds + ' millisecond' + ((seconds == 1) ? '' : 's'));
+        if (minutes > 0) segments.push(minutes + ' minute' + ((minutes == 1) ? '' : 's'))
+        // If Uptime is less than a minute, push said String to segments
+        else segments.push("Less than a minute");
+        
         const dateString = segments.join(', ');
-        
-        
         await interaction.reply(dateString.toString());
     },
 };
