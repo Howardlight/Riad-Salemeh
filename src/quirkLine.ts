7682fd4f90abd670a335a91950fc0de5e4dd8d93
Ciel/howard <howardlight+github@protonmail.com>
2022-01-17 18:29:26 +0200

    exported quirklines to their own file

---
+++ src/quirkLine.ts
@@ -0,0 +1,8 @@
+export const quirkline = [
+    "El lira b2alf 5er tfarrage:",
+    "I did some handaset, and the new Lira value is:",
+    "Your local serraf says:",
+    "Ana ma 5assni, bss llira lyom:",
+    "Bisallem 3lek Michel Aoun:",
+    "Discount 51%, bss la 2elak:",
+]
