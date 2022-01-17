e60cf781cb3766e2857d3bd31ca4a1d3b0cf0e3b
Ciel/howard <howardlight+github@protonmail.com>
2022-01-17 10:13:44 +0200

    Added getRate.ts

---
+++ src/getRate.ts
@@ -0,0 +1,29 @@
+const Cheerio = require("../node_modules/cheerio");
+const fetch = require("../node_modules/node-fetch");
+
+// use Fetch to get the website 
+const getRawData = (URL: string) => {
+    return fetch(URL)
+    .then((response: any) => response.text())
+    .then((data: string) => {
+        return data;
+    });
+};
+
+// website, might modify later
+const URL: string = "https://lbprate.com/";
+
+// Process the website then return List
+// of desired values
+export const getWebsiteData = async () => {
+    const lbpRaw = await getRawData(URL);
+    const parsedData = Cheerio.load(lbpRaw);
+
+    let list: string[] = []; // i refers to the index, e refers to the element
+    parsedData(".text-white").each(function (i: any, e: any) {
+        list[i] = parsedData(e).text();
+    });
+    const out:string[] = [list[0], list[2], list[4]];
+
+    return out;
+};
