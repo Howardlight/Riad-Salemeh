dbad55b6ea4d649ecb63f336620a229a52222f4b
Ciel/howard <howardlight+github@protonmail.com>
2022-02-22 23:15:16 +0200

    Cleaned up lbprate command

--- src/commands/lbprate.ts
+++ src/commands/lbprate.ts
@@ -9,11 +9,14 @@ export const lbprate: CommandInt = {
     description: "use Fetch and Cheerio to get the LBP rate",
     cooldown: 30,
     run: async (message: Message) => {
-        // console.log(rateData[0]);
 
-        // create the response, the extended response, concat then return
-        const response = `${quirkline[Math.floor(Math.random()*quirkline.length)]}`;
-        const extendedResponse = (`\nThe BUY rate is ${rateData[1]}\nThe SELL rate is ${rateData[2]}\n${rateData[0]}`);
-        await message.channel.send(response.concat(extendedResponse));
+        if(rateData[0] === "NULL") {
+            await message.channel.send("This command is currently unavailable.");
+        } else {
+            // create the response, the extended response, concat then return
+            const response = `${quirkline[Math.floor(Math.random()*quirkline.length)]}`;
+            const extendedResponse = (`\nThe BUY rate is ${rateData[1]}\nThe SELL rate is ${rateData[2]}\n${rateData[0]}`);
+            await message.channel.send(response.concat(extendedResponse));
+        }
     }
 }
