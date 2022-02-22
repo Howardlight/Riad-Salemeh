3db512a67d038b83206b64d5d123100d84ec8e8f
Ciel/howard <howardlight+github@protonmail.com>
2022-02-22 23:15:06 +0200

    Revamped lirarate command to use Caching method

--- src/commands/lirarate.ts
+++ src/commands/lirarate.ts
@@ -3,29 +3,33 @@ const fetch = require("../../node_modules/node-fetch");
 import { Message } from "discord.js";
 import { CommandInt } from "../interfaces/CommandInt";
 import {
-    getLirarateAPIData,
     timeDiffCalc,
     getEESTTime,
 } from "../utils/utils";
+import { quirkline } from "../quirkLine";
+import { liraData } from "..";
 
 export const lirarate: CommandInt = {
     name: "lirarate",
-    description: "use Fetch and Cheerio to get the LBP rate",
+    description: "Returns LBP rate from Lirarate.com",
     run: async (message: Message) => {
 
         const nowDate: Date = getEESTTime(2);
-        const rates = await getLirarateAPIData(nowDate);
-        if(!("buy" in rates)) console.error(`Lirarate | getAPIData | rates | VALUE IS NULL`);
-        else {
+        const rates = liraData;
+
+        if (rates.buy.length === 0) {
+            console.error(`Lirarate | getAPIData | rates | VALUE IS NULL`, rates);
+            await message.channel.send("This command is currently unavailable.");
+        } else {
 
             // get values
-            const buyRate = rates["buy"].pop();
-            const sellRate = rates["sell"].pop();
+            const buyRate = rates["buy"].slice(-1);
+            const sellRate = rates["sell"].slice(-1);
 
 
             // These 2 should be the same, no need to store them both
-            const buyDate: Date = new Date(buyRate[0]);
-            // const sellDate: Date = new Date(sellRate[0]); 
+            const buyDate: Date = new Date(buyRate[0][0]);
+            // const sellDate: Date = new Date(sellRate[0][0]);
 
 
             // console.log(sellDate.toLocaleTimeString());
@@ -34,19 +38,9 @@ export const lirarate: CommandInt = {
             // console.log(timeDiffCalc(buyDate, nowDate));
             const timeDiff: string = timeDiffCalc(buyDate, nowDate);
 
-            // quirklines on the top
-            const quirkline = [
-                "El lira b2alf 5er tfarrage:",
-                "I did some handaset, and the new Lira value is:",
-                "Your local serraf says:",
-                "Ana ma 5assni, bss llira lyom:",
-                "Bisallem 3lek Michel Aoun:",
-                "Discount 51%, bss la 2elak:",
-            ];
-
             // create the response, the extended response, concat then return
             const response = `${quirkline[Math.floor(Math.random() * quirkline.length)]}`;
-            const extendedResponse = (`\nThe BUY rate is ${buyRate[1]}\nThe SELL rate is ${sellRate[1]}\n Updated ${timeDiff} ago`);
+            const extendedResponse = (`\nThe BUY rate is 1 USD at ${buyRate[0][1]} LBP\nThe SELL rate is 1 USD at ${sellRate[0][1]} LBP\n Updated ${timeDiff} ago`);
             await message.channel.send(response.concat(extendedResponse));
         }
     }
