24c5fac41c90d048deb8a59ca3a87a10fb2400e7
HowardLight <howardlight@protonmail.com>
2022-05-25 20:54:05 +0300

    Added fuel command to commandList

--- src/commands/_CommandList.ts
+++ src/commands/_CommandList.ts
@@ -6,6 +6,7 @@ import { lbprate } from "./lbprate";
 import { help } from "./help";
 import { getUptime } from "./getUptime";
 import { convert } from "./convert";
+import { fuel } from "./fuel";
 // import { lirarate } from "./lirarate";
 
-export const CommandList: CommandInt[] = [github, link, list, lbprate, help, getUptime, convert];
+export const CommandList: CommandInt[] = [github, link, list, lbprate, help, getUptime, convert, fuel];
