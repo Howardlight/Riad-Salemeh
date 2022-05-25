ad6f134ab2d5a0e4336478c411ebdb29575e5b89
HowardLight <howardlight@protonmail.com>
2022-05-25 20:53:07 +0300

    Created ResponseInt.ts

---
+++ src/interfaces/ResponseInt.ts
@@ -0,0 +1,7 @@
+
+interface FuelResponse {
+    name: string;
+    data: Array<number[]>;
+}
+
+//TODO: Add enumerator or whatever for OCTANE 95 (LBP) | OCTANE 98 (LBP) | DIESEL (LBP) | GAS (LBP) | BRENT CRUDE OIL (USD)
