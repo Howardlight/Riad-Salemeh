956d2ccf6dc86fd8ca140470e6c769c840b49787
HowardLight <howardlight@protonmail.com>
2022-05-25 20:53:54 +0300

    Created fuel Command

---
+++ src/commands/fuel.ts
@@ -0,0 +1,22 @@
+import {Message} from "discord.js";
+import {CommandInt} from "../interfaces/CommandInt";
+import { getLatestFuel } from "../utils/utils";
+
+export const fuel: CommandInt = {
+    name: "fuel",
+    description: "Get price of fuel in LL",
+    run: async (message: Message) => {
+
+        try{
+
+            const fuel = await getLatestFuel();
+
+
+            await message.channel.send(`Octane 95: ${fuel[0]}LL\nOctane 98: ${fuel[1]}LL\nDiesel: ${fuel[2]}LL\nGas: ${fuel[3]}LL\nCrude Oil: ${fuel[4]}$`);
+
+        } catch(error) {
+            console.log(error);
+            await message.channel.send("This command is currently unavailable.");
+        }
+    }
+}
