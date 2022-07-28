dad52de864bc16b32a8685d72df19b43ed9c33fd
HowardLight <howardlight@protonmail.com>
2022-07-28 11:46:06 +0300

    Added Metrics to fuel.ts

--- src/commands/fuel.ts
+++ src/commands/fuel.ts
@@ -13,7 +13,7 @@ export const fuel: CommandInt = {
                 return ;
             }
 
-            await message.channel.send(`Octane 98: ${fuelRate[1]}\nOctane 95: ${fuelRate[2]}\nGas: ${fuelRate[3]}\nDiesel: ${fuelRate[4]}\nCrude Oil: ${fuelRate[5]}\n${fuelRate[0]}`);
+            await message.channel.send(`Octane 98: ${fuelRate[1]} per 20L\nOctane 95: ${fuelRate[2]} per 20L\nGas: ${fuelRate[3]} per (LPG)10KG\nDiesel: ${fuelRate[4]} per 20L\nCrude Oil: ${fuelRate[5]}\n${fuelRate[0]}`);
 
         } catch(error) {
             console.log(error);
