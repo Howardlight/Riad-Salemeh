6c37048374165661d877f701e9975e2276cad7ab
HowardLight <howardlight+github@protonmail.com>
2022-08-04 15:07:10 +0300

    Completed Phase out of Message Commands

--- src/commands/github.ts
+++
@@ -1,9 +0,0 @@
-import { Message } from "discord.js";
-import { CommandInt } from "../interfaces/CommandInt";
-
-
-export const github: CommandInt = {
-    name:"github",
-    description:"Links to the github of the bot",
-    run: async (message: Message) => {message.channel.send("https://github.com/Howardlight/Riad-Salemeh-TS");}
-}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-import { Client, GatewayIntentBits, Partials, Collection, Interaction, Message, CommandInteraction } from "discord.js";
+import { Client, GatewayIntentBits, Collection, Interaction } from "discord.js";
 import { validateEnv } from "./utils/validateEnv";
-import { onMessage } from "./events/onMessage";
 import { onInteraction } from "./events/onInteraction";
 import { getWebsiteData } from "./utils/getRate";
 
@@ -18,7 +17,6 @@ export const cooldowns = new Collection();
     // if assertion fails, stops the bot
     if(!validateEnv) return;
 
-    const PREFIX = process.env.PREFIX as string;
     const client = new Client(
         { intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.DirectMessages]}
     );
@@ -30,18 +28,10 @@ export const cooldowns = new Collection();
         interactions.set(command.data.name, command);
     }
 
-
     // EVENT LISTENERS
     client.once("ready", ()=> {
         client.user?.setActivity("with your Dollars");
         console.log(`Logged in as ${client.user?.username}! | ${client.user?.id}`);
-    });
-
-    client.on("messageCreate", async (message: Message) => {
-        if(!message.content.startsWith(PREFIX)) return ;
-        // creates the Argument List, then passes it with the message to Event Handler 
-        var args: string[] = message.content.slice(PREFIX.length).trim().split(/ +/);
-        await onMessage(message, args);
     });
 
     client.on('interactionCreate', async (interaction: Interaction) => {
